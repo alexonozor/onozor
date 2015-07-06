1b36e92e0b940d1faa39c8e244fcf4a0b5b3372e
andela-obogbare <alexonozor@gmail.com>
2015-07-06 20:06:49 +0100

    assets compiled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   #filters
+
+ if Rails.env == "production"
+   puts "Am on production"
+ else
+  puts "NO..."
+ end
   
   before_filter :last_requested_at
   before_action :load_users
