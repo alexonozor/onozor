65a809fffb4228f0db22be052c7e6fbbe5da66ef
andela-obogbare <alexonozor@gmail.com>
2015-09-01 15:56:33 +0100

    configure redis on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,18 @@ NairaOverflow::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "heroku.com",
+      :user_name            => ENV['SENDGRID_USERNAME'],
+      :password             => ENV['SENDGRID_PASSWORD'],
+      :authentication       => 'plain',
+      :openssl_verify_mode  => 'none',
+      :enable_starttls_auto => true  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
