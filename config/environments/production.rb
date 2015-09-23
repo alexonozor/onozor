061d2878563076f71d599fff631dc8e25a950626
andela-obogbare <alexonozor@gmail.com>
2015-09-23 12:54:06 +0100

    configure mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ NairaOverflow::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
-      :port                 => 587,
+      :port                 => 465,
       :domain               => 'alexinquire.herokuapp.com',
       :user_name            => ENV['email'],
       :password             => ENV['password'],
