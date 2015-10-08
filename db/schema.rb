66ab6dcc3c19e682d5ad2a2962b82d9b82a2dfba
Innocent Amadi <innocent.amadi@andela.com>
2015-10-08 22:37:14 +0100

    revamps user profile

--- db/schema.rb
+++ db/schema.rb
@@ -90,13 +90,6 @@ ActiveRecord::Schema.define(version: 20151007151513) do
     t.datetime "updated_at"
   end
 
-  create_table "followers", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "follow"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
     t.integer  "sluggable_id",              null: false
