fca9f5792c08e1c4223034b8dcc61a9577baf006
andela-obogbare <alexonozor@gmail.com>
2015-10-20 00:51:19 +0100

    Added sorting and fix alert to toster with webhook

--- app/models/question.rb
+++ app/models/question.rb
@@ -100,7 +100,7 @@ class Question < ActiveRecord::Base
 
 
   def send_slack_message
-    SLACK_NOTIFIER.ping("New Question from #{self.user.username} :
+    SLACK_NOTIFIER.ping("<!channel> New Question from #{self.user.username} :
                  #{self.name} <http://www.onozor.com/questions/#{self.slug}|Click here> to answer", http_options: { open_timeout: 5 })
   end
 end
