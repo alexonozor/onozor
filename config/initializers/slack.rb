0affbfec62f1b2a140061c2afa00441489ace089
andela-obogbare <alexonozor@gmail.com>
2015-10-20 01:44:02 +0100

    change webhook name

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,3 +1,3 @@
 SLACK_NOTIFIER = Slack::Notifier.new("https://hooks.slack.com/services/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",
-    channel: "#onozor", username: "@alexonozor"
+    channel: "#onozor", username: "ONOZOR"
 )
