5f7e9b0e2a64c1cfbb95ab666bdd67214096fa73
alexonozor <alexonozor@gmail.com>
2016-10-25 02:14:19 +0100

    reset the configuration

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,20 +1,15 @@
-# set path to application
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-working_directory app_dir
+root = "/home/deploy/apps/onozor/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
 
-
-# Set unicorn options
+listen "/tmp/unicorn.onozor.sock"
 worker_processes 2
-preload_app true
 timeout 30
 
-# Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
-
-# Logging
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
-
-# Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
